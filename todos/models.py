10b016a2297bd8eabbf281256895e0958e5b7c15
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-13 15:45:34 +0000

    added models for Category, Event, Todo

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    label = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    due_date = models.DateTimeField('date due')
+    status = models.BooleanField(True, False)
+    HIGH = 'H'
+    MED = 'M'
+    LOW = 'L'
+    PRIORITY_CHOICES = [
+        (HIGH , 'High'),
+        (MED, 'Medium'),
+        (LOW, 'Low'),
+    ]
+    priority = models.CharField(
+        choices=PRIORITY_CHOICES,
+        default=MED,
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    event_time = models.DurationField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Category(models):
+    title = models.CharField(max_length=200)
+    RED = 'RD'
+    BLUE = 'BL'
+    GREEN = 'GN'
+    BLACK = 'BK'
+    WHITE = 'WT'
+    COLOR_CHOICES = [
+        (RED , 'Red'),
+        (BLUE, 'Blue'),
+        (GREEN, 'Green'),
+        (BLACK, 'Black'),
+        (WHITE, 'White'),
+    ]
+    color = models.CharField(
+        choices=COLOR_CHOICES,
+        default=WHITE,
+    )
