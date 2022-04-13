baab597d9aeacf5b7c970298243e8cf124963676
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-13 23:56:26 +0000

    added CSRF Trusted origins to settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'myproject',
     'todo.apps.TodoConfig',
+    'myproject',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -72,6 +72,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.gitpod.io',
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
