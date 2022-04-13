446e72836da4f82293541c9b58c411be6a08720f
Zachary Kestner <zachary.kestner@student.awesomeinc.org>
2022-04-13 23:23:19 +0000

    fixing issue with urls to proceed with created superuser

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Todo, Event, Category
+# Register models here
+admin.site.register(Todo)
+admin.site.register(Event)
+admin.site.register(Category)
